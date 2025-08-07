9fd4dd57b555e9cfe1e001737e174ca24c5208ab
Vitaly <vitaliy.zayakin@gmail.com>
2025-08-07 09:49:28 +0500

    feat: add not-found page

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,10 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiBlockStatus } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [TuiBlockStatus],
   templateUrl: './not-found.html',
   styleUrl: './not-found.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NotFound {}
+export class NotFound {
+  protected readonly message = 'Page not found';
+  protected readonly imageSrc = './images/not-found.svg';
+}
