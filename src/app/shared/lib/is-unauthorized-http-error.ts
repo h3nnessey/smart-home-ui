3b57628b537dd7d490fa58e5a144f89c8efea82d
Vitaly <vitaliy.zayakin@gmail.com>
2025-08-18 18:34:30 +0500

    refactor(is-unauthorized-http-error): add number typecheck

--- src/app/shared/lib/is-unauthorized-http-error.ts
+++ src/app/shared/lib/is-unauthorized-http-error.ts
@@ -4,10 +4,13 @@ import type { UnauthorizedHttpError } from '@typings/api/interfaces';
 export const isUnauthorizedHttpError = (
   error: unknown,
 ): error is UnauthorizedHttpError => {
+  if (typeof error !== 'object' || error === null) {
+    return false;
+  }
+
   return (
-    typeof error === 'object' &&
-    error !== null &&
     'status' in error &&
+    typeof error.status === 'number' &&
     error.status === HttpErrors.Unauthorized
   );
 };
