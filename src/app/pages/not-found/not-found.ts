763e46b38169a7c9d86b3df67e18b7988f3bcf31
Vitaly <vitaliy.zayakin@gmail.com>
2025-08-20 13:03:16 +0500

    refactor(not-found): replace hard-coded routerLink value with AppRoutes value

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -3,6 +3,7 @@ import { RouterLink } from '@angular/router';
 import { TuiButton } from '@taiga-ui/core';
 import { TuiBlockStatus } from '@taiga-ui/layout';
 import { AuthService } from '@services/auth-service';
+import { AppRoutes } from '@shared/config/app-routes';
 
 @Component({
   selector: 'app-not-found',
@@ -14,6 +15,7 @@ import { AuthService } from '@services/auth-service';
 export class NotFound {
   private readonly authService = inject(AuthService);
 
+  protected readonly link = AppRoutes.Login;
   protected readonly message = 'Page not found';
   protected readonly imageSrc = './images/not-found.svg';
 
